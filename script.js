37bc9db55cd118d77c7370c218817fbed4667279
Helio Trope <petladda.94@gmail.com>
2023-11-23 20:25:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+window.onscroll = () =>{
+
+  searchForm.classList.remove('active');
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+}
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .icons-container').classList.add('active');
+  }else{
+    document.querySelector('.header .icons-container').classList.remove('active');
+  }
+
+  fadeOut();
+
+}
+
+var swiper = new Swiper(".new-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".featured-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var cart = $(".cart");
+var cart_number = $(".cart_number");
+var close_id;
+
+$(".item").each(function(index){
+	$(this).click(function(e){
+		  e.stopPropagation();
+		  if ($('.dropdown').find('.dropdown-menu').is(":hidden")){
+			$('.dropdown-toggle').dropdown('toggle');
+		  }
+		 addToCart(index);
+	});
+});
